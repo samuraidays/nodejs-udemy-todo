9f9df9d8c2e5e0606ef47f00f19c30ad4bba5573
12ban <12ban@12mac.local>
2023-03-17 09:44:07 +0800

    31 done

--- app/app.js
+++ app/app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
 const taskRoute = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
 
 const PORT = 8100;
 
+// ルーティング
 app.use("/api/v1/tasks", taskRoute);
 
-app.listen(PORT, console.log("サーバが起動しました"));
+// データベース接続
+const start  = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+        // サーバ起動
+        app.listen(PORT, console.log("サーバが起動しました"));
+    } catch(err) {
+        console.log(err);
+    }
+};
 
+start();

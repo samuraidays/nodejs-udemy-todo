ab92b550a8716864e2a93d6adb00487035320b33
12ban <12ban@12mac.local>
2023-03-19 17:07:06 +0800

    50 done

--- app/public/script.js
+++ app/public/script.js
@@ -1,11 +1,18 @@
 const tasksDOM = document.querySelector(".tasks");
 const formDOM = document.querySelector(".task-form")
 const taskInputDOM = document.querySelector(".task-input")
+const formAlertDOM = document.querySelector(".form-alert")
 
 // 全タスク表示
 const showTasks = async () => {
     try {
         const { data: tasks } = await axios.get("/api/v1/tasks")
+
+        // タスクがないとき
+        if(tasks.length < 1){
+            tasksDOM.innerHTML = `<h5 class="empty-list">タスクがありません</h5>`
+            return;
+        }
 
         const allTasks = tasks.map((task) => {
             const { completed, _id, name } = task;
@@ -39,9 +46,18 @@ formDOM.addEventListener("submit", async(event) => {
         await axios.post("/api/v1/tasks", { name: name } );
         showTasks();
         taskInputDOM.value = "";
+        formAlertDOM.style.display = "block";
+        formAlertDOM.textContent = "タスクを追加しました";
+        formAlertDOM.classList.add("text-success");
     } catch (error) {
-        console.log(err)
+        console.log(error)
+        formAlertDOM.style.display = "block";
+        formAlertDOM.innerHTML = "20文字を超えています。20文字以内で入力してください";
     }
+    setTimeout(() => {
+        formAlertDOM.style.display = "none";
+        formAlertDOM.classList.remove("text-success");
+    }, 3000);
 })
 
 // タスクを削除
